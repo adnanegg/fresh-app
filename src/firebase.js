578a4c8312b8519649f318a1524a15fe306ea9aa
root <root@DESKTOP-9V7NCFJ.>
2025-02-19 14:48:46 +0100

    improved security

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,8 @@ import { getDatabase } from "firebase/database"; // Correct import for Realtime 
 import { getStorage } from "firebase/storage";
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDVR7DvWepRUcMXGsDQbm9_gJIxAVxXKkE",
-//     authDomain: "task-game-65770.firebaseapp.com",
-//     databaseURL: "https://task-game-65770-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "task-game-65770",
-//     storageBucket: "task-game-65770.firebasestorage.app",
-//     messagingSenderId: "673738120791",
-//     appId: "1:673738120791:web:b8c85fb9b36a2ca7528d12"
-//   };
+
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
